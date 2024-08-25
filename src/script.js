05960f34b9110dcc11bd3f59dda9444385de0947
SandSide <37347875+SandSide@users.noreply.github.com>
2024-08-25 17:17:51 +0100

    feat: Create grid

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
-function createGrid(width,height, size){
+function createGrid(width, height, size){
 
-    // Config container
+    // Init container
     const gridContainer = document.getElementById('grid');
     gridContainer.style.gridTemplateColumns = `repeat(${width},${size})`;
     gridContainer.style.gridTemplateRows = `repeat(${height},${size})`;
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
+    // Init grid
+    const gridValues = Array.from({ length: height }, () => Array(width).fill(0));
+    const gridElements = Array.from({ length: height }, () => Array(width).fill(null));
+
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
 
             // Create grid node
             const node = document.createElement('div');
@@ -14,11 +19,33 @@ function createGrid(width,height, size){
             node.style.width = size;
             node.style.height = size;
 
-            gridContainer.appendChild(node);
+            gridContainer.appendChild(node)
+            gridElements[row][col] = node;
         }
     }
+
+    return { gridValues, gridElements };
+}
+
+function update(gridValues, gridElements){
+
+    // while(true){
+
+        for (let row = 0; row < gridValues.length; row++) {
+            for (let col = 0; col < gridValues[row].length; col++) {
+                
+                if (row % 2 == 0)
+                    gridElements[row][col].style.backgroundColor = 'gray';
+            }
+            
+        }
+
+
+    // }
 }
 
 window.onload = function(){
-    createGrid(100,40,'10px');
+    const { gridValues, gridElements } = createGrid(100, 40, '10px');
+
+    // update(gridValues, gridElements)
 }
